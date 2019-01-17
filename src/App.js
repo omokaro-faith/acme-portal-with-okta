d5c41841668e4e145b0a266fe81c5317d128375b
andela-fomokaro <faith.omokaro@andela.com>
2019-01-17 10:30:34 +0100

    add signin widget for Acme portal, set env variables

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Security, SecureRoute, ImplicitCallback } from '@okta/okta-react';
 
-import './App.css';
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import Staff from './components/pages/Staff';
+import LogIn from './components/auth/LogIn';
+
+import './App.css';
+
+const onAuthRequired = ({history}) => {
+  history.push('/login');
+}
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <div className="App">
-          <Navbar />
-            <Route path="/" exact component={Home} />
-            <Route path="/staff" component={Staff} />
-        </div>
+        <Security issuer={process.env.REACT_APP_BASE_URL}
+                client_id={process.env.REACT_APP_CLIENT_ID}
+                redirect_uri={window.location.origin + process.env.REACT_APP_CALLBACK_REDIRECT}
+                onAuthRequired={onAuthRequired} >
+          <div className="App">
+            <Navbar />
+            <div className="container">
+              <Route path="/" exact component={Home} />
+              <SecureRoute path="/staff" component={Staff} />
+              <Route path='/login' 
+                render={() => <LogIn
+                baseUrl={process.env.REACT_APP_BASE_URL} />} />
+              <Route path={process.env.REACT_APP_CALLBACK_REDIRECT} component={ImplicitCallback} />
+            </div>
+          </div>
+        </Security>
       </Router>
     );
   }
--- src/components/pages/Staff.js
+++ src/components/pages/Staff.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react'
 
-export default () => {
-  return (
-    <div>
-      <h1> Staff </h1>
-    </div>
-  );
-};
+class Staff extends Component {
+  constructor(){
+    super();
+    this.state = {
+      currentName: "",
+      currentUserEmail: ""
+    }
+  }
+
+  componentDidMount() {
+    const idToken = JSON.parse(localStorage.getItem('okta-token-storage'));
+    this.setState({
+      currentName: idToken.idToken.claims.name,
+      currentUserEmail: idToken.idToken.claims.email,
+    });
+  }
+  render () {
+    const { currentName, currentUserEmail } = this.state;
+    return (
+      <div>
+        <h1> Welcome { currentName }</h1>
+        <p> Email: { currentUserEmail } </p>
+        <p> You have reached the authorized staff area of the portal </p>
+      </div>
+    )
+  }
+}
+
+export default Staff
